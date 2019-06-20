3f1e0c3422b9154326d53491f564564f69ad0f4b
Alexandre Ferreira <hello@alexontheweb.me>
2019-06-20 19:03:21 +0100

    Users can now jump to the previous discussion Add style to buttons

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 function scrollToNextTarget(targetElement) {
-  targetElement.scrollIntoView({ block: 'start' });
+  targetElement[currentTargetIndex].scrollIntoView({ block: 'start' });
   window.scrollBy(0, -300);
+
+  highlightTarget(targetElement);
 }
 
 function highlightTarget(targetElement) {
@@ -20,25 +22,87 @@ function incrementTargetIndex(targetLimit) {
   }
 }
 
-function onClick() {
+function subtractTargetIndex(targetLimit) {
+  const lastIndex = targetLimit - 1;
+
+  if (currentTargetIndex === 0) {
+    currentTargetIndex = lastIndex;
+  } else {
+    currentTargetIndex--;
+  }
+}
+
+function handleClick(type) {
   /*
     Opted to get the elements in the event because Github still loads content
     after all the DOM is "officially" rendered
   */
-  const targetElement = document.querySelectorAll('#files .inline-comments.js-inline-comments-container');
-  const totalTargets = targetElement.length;
-  const currentTarget = targetElement[currentTargetIndex];
 
-  scrollToNextTarget(currentTarget);
-  highlightTarget(currentTarget);
-  incrementTargetIndex(totalTargets);
+  const targetElement = document.querySelectorAll('#files .inline-comments.js-inline-comments-container');
+  const totalTargetsNumber = targetElement.length;
+
+  if (type === 'next') {
+    currentTargetIndex !== null ? incrementTargetIndex(totalTargetsNumber) : (currentTargetIndex = 0);
+  } else {
+    currentTargetIndex !== null
+      ? subtractTargetIndex(totalTargetsNumber)
+      : (currentTargetIndex = totalTargetsNumber - 1);
+  }
+
+  return scrollToNextTarget(targetElement);
 }
 
-const wrapper = document.createElement('section');
-let currentTargetIndex = 0;
+function displayButtonContainer() {
+  const containerStyle = `
+    position: absolute;
+    top: 50%;
+    right: 180px;
+    width: 80px;
+    display: flex;
+    justify-content: space-between;
+    transform: translateY(-50%);
+  `;
 
-wrapper.innerHTML = '<button id="foobar" onclick="onClick()">Next -></button>';
+  let wrapper = document.createElement('div');
+  wrapper.style.cssText = containerStyle;
+
+  return wrapper;
+}
+
+function displayButtons() {
+  const prevIconUrl = chrome.extension.getURL('assets/prev_icon.png');
+  const nextIconUrl = chrome.extension.getURL('assets/next_icon.png');
+
+  const buttonStyle = `
+    width: 28px;
+    height: 28px;
+    background-size: 100% 100%;
+    border: 1px solid #D5D6D6;
+    border-radius: 4px;
+    cursor: pointer;
+  `;
+
+  const prevStyle = `
+    background: url('${prevIconUrl}') #FFFFFF no-repeat center;
+  `;
+
+  const nextStyle = `
+    background: url('${nextIconUrl}') #FFFFFF no-repeat center;
+  `;
+
+  return `
+    <input type="button" id="github_extension_prevButton" style="${buttonStyle} ${prevStyle}" />
+    <input type="button" id="github_extension_nextButton" style="${buttonStyle} ${nextStyle}" />
+  `;
+}
+
+let currentTargetIndex = null;
+const wrapper = displayButtonContainer();
+
+wrapper.classList.add('foobar');
+wrapper.innerHTML = displayButtons();
 
 document.querySelector('.pr-toolbar').append(wrapper);
 
-document.querySelector('#foobar').addEventListener('click', () => onClick());
+document.querySelector('#github_extension_prevButton').addEventListener('click', () => handleClick('prev'));
+document.querySelector('#github_extension_nextButton').addEventListener('click', () => handleClick('next'));
